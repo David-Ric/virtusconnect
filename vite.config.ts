d739dfd222dfffe750b35ddca1343e6645a0219c
David_Ric <dric6996@gmail.com>
2024-01-21 21:45:37 -0300

    ajustes2

--- vite.config.ts
+++ vite.config.ts
@@ -34,8 +34,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    // Outros plugins, se houver
-  ],
+  plugins: [react()],
+  base:"/virtusconnect",
 });

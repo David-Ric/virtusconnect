4ff4b29a0799d66a29f1e8679a66b9f71ced8237
David_Ric <dric6996@gmail.com>
2024-01-21 22:50:55 -0300

    ajistes

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,28 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./router/Routes";
+// import './styles/global.scss';
+
+// ReactDOM.createRoot(document.getElementById("root")!).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./router/Routes";
-import './styles/global.scss';
+import "./styles/global.scss";
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/virtusconnect/service-worker.js")
+    .then((registration) => {
+      console.log("Service Worker registrado com sucesso:", registration);
+    })
+    .catch((error) => {
+      console.log("Erro ao registrar o Service Worker:", error);
+    });
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
